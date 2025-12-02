c787ce462c1799cbf08101986f0ee558a8939178
Nangmanful <89606590659@naver.com>
2025-12-02 20:37:12 +0900

    [update] CICD

---
+++ src/test/java/com/skax/physicalrisk/PhysicalRiskManagementApplicationTests.java
@@ -0,0 +1,15 @@
+package com.skax.physicalrisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("local")
+class PhysicalRiskManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // 애플리케이션 컨텍스트가 정상적으로 로드되는지 검증
+    }
+}
